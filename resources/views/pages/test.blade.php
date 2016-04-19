c5a5372d347c659ca4fd55c9a8fa3974e6a4574e
Mathew Harrington <mathewharrington88@gmail.com>
2016-04-19 16:03:32 +1000

    adjusted layout

--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -1,8 +1,23 @@
+<?php
+// counter for rows.
+$i = 0;
+$j = 0;
+ ?>
+
 @extends('layout')
 @section('content')
-<h1>Go Outside</h1>
+<h3>Go Outside</h3>
 @foreach($results as $result)
-<img src="{{ $result }}">
+<?php if(($i % 3) === 0)
+      {
+         echo "<div class='row'>";
+         $j = 0;
+      }
+?>
+<div class="one-third column">
+<img src="{{ $result }}" class="u-max-full-width">
+</div>
+<?php if($j === 2) echo "</div>"; ?>
+<?php $i++; $j++ ?>
 @endforeach
-
 @stop
