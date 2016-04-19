c3efa365a61eb00bcc0b0749cd871f18601e306c
Mathew Harrington <mathewharrington88@gmail.com>
2016-04-19 15:04:50 +1000

    added skeleton framework

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,35 +3,8 @@
    <head>
       <meta charset="UTF-8">
       <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-      <style>
-          html, body {
-             height: 100%;
-          }
-
-          body {
-             margin: 0;
-             padding: 0;
-             width: 100%;
-             display: table;
-             font-weight: 100;
-             font-family: 'Lato';
-          }
-
-          .container {
-             text-align: center;
-             display: table-cell;
-             vertical-align: middle;
-          }
-
-          .content {
-             text-align: center;
-             display: inline-block;
-          }
-
-          .title {
-             font-size: 96px;
-          }
-      </style>
+      <link rel="stylesheet" href="/css/normalize.css" type="text/css">
+      <link rel="stylesheet" href="/css/skeleton.css" type="text/css">
       <title>Document</title>
    </head>
    <body>
