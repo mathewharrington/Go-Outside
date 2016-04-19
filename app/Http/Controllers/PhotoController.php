abb69b5446407fe81c32c9dc82c3dda3f5c03a5d
Mathew Harrington <mathewharrington88@gmail.com>
2016-04-19 14:22:07 +1000

    removed hardcoded keys/urls - issue #2

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -1,12 +1,16 @@
 <?php
+/*
+* Photo controller class. Currently only deals with images sourced from flickr,
+* may need to add more functions to handle 500px eventually. Not sure about
+* naming the function 'load'
+*
+* @author Mathew Harrington
+*/
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
-
-// adding my namespaces
+// my namespaces
 use App\Pest;
 use App\API_Adapter;
 use App\Result;
@@ -15,22 +19,19 @@ class PhotoController extends Controller
 {
    public function load()
    {
-      // have these here for now until I figure out where to put it
-      define("FLICKR_BASE_URL", "https://api.flickr.com/services/rest/?method=flickr.photos.search");
-      define("FLICKR_API_KEY", "75c703128e789bbbb1cad18b34448032");
-
-      // instantiate objects and make request
-      $FlickrClient = new \App\Pest\Pest(FLICKR_BASE_URL);
+      // setup objects to make flickr request
+      $FlickrClient = new \App\Pest\Pest(getenv('FLICKR_BASE_URL'));
       $FlickrAPI = new \App\API_Adapter\FlickrAPI($FlickrClient);
       $FlickrResultSet = new \App\Result\FlickrResultSet();
 
       // add options for flickr query
       $FlickrAPI->setOption("tags", "melbourne");
-      $FlickrAPI->setOption("api_key", FLICKR_API_KEY);
+      $FlickrAPI->setOption("api_key", getenv('FLICKR_API_KEY'));
       $FlickrAPI->setOption("per_page", 20);
 
       $flickrResponse = $FlickrAPI->get();
       $FlickrResultSet->addResults($flickrResponse);
+      // this step is important - pest seems to add a few of its own characters to the response
       $FlickrResultSet->parse();
 
       $photoURLs = $FlickrResultSet->buildPhotoURLArray();
