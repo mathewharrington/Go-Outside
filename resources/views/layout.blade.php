0d961937947575ad7a68b4999c2656f5aeb908a7
Mathew Harrington <mathewharrington88@gmail.com>
2016-04-19 17:55:09 +1000

    adjusted layout some more

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
    <head>
       <meta charset="UTF-8">
-      <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+      <link href='https://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
       <link rel="stylesheet" href="/css/normalize.css" type="text/css">
       <link rel="stylesheet" href="/css/skeleton.css" type="text/css">
       <title>Document</title>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
 @extends('layout')
 
 @section('content')
-   <h1>Welcome Page</h1>
+<section class="header">
+   <h2>Go Outside</h2>
+</section>
+<form>
+   <div class="row">
+      <div class="six columns">
+         <label for="cityInput">City Name</label>
+         <input class="u-full-width" type="text">
+      </div>
+      <div class="six columns">
+         <label for=""></label>
+      </div>
+   </div>
+   <div class="row">
+      <input class="button-primary" type="submit" value="Submit">
+   </div>
+</form>
 @stop
