9d25f1bdde1d0f59b5532964e3af7eeab12455ef
Mathew Harrington <mathewharrington88@gmail.com>
2016-05-04 12:28:11 +1000

    parsing is sorted, issue #7, still wanting to add more methods but this will be fine for now

--- app/Result/OpenWeatherResultSet.php
+++ app/Result/OpenWeatherResultSet.php
@@ -1,5 +1,9 @@
 <?php
 /*
+* Class models the json response from OpenWeather. Used to format the response
+* into useable data, primarily a weather description for use in searching for
+* images.
+*
 * @author Mathew Harrington
 */
 
@@ -13,24 +17,65 @@ class OpenWeatherResultSet implements iResultSet {
 
    function __construct()
    {
-      //TODO
+      // TODO?
    }
 
-   public function count()
-   {
-      // TODO
-   }
-
+   /*
+   * Function to parse the raw json response into a useable array.
+   *
+   * @return Array An array of returned json data
+   */
    public function parse()
    {
-      //TODO
+      $this->parsedResponse = json_decode($this->rawResponse, true);
    }
 
+   /*
+   * Adds response to member variable.
+   *
+   * @param Array The response from REST request
+   */
    public function addResults($results)
    {
       $this->rawResponse = $results;
-      print($results);
    }
 
+   /*
+   * Function to get weather description from parsed json response.
+   *
+   * @return String A short description of current weather
+   */
+   public function getShortWeatherDesc()
+   {
+      return $this->parsedResponse['weather'][0]['main'];
+   }
+
+   /*
+   * Function to get a 'slightly' longer description of current weather.
+   *
+   * @return String A slightly longer string for description
+   */
+   public function getWeatherDesc()
+   {
+      return $this->parsedResponse['weather'][0]['description'];
+   }
+
+   /*
+   * @return String The name of the city featured in the response
+   */
+   public function getCityName()
+   {
+      return $this->parsedResponse['name'];
+   }
+
+   /*
+   * Function to return the intire parsed json response to do what you like with.
+   *
+   * @return Array An array of json data
+   */
+   public function getParsedResponse()
+   {
+      return $this->parsedResponse;
+   }
 }
 ?>
