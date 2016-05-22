9f548492e1456bde5e527824e187a9309e2e76b1
Mathew Harrington <mathewharrington@Mathews-MacBook-Pro.local>
2016-05-22 19:37:04 +1000

    messing around with the form still, internet playing up so only got half way

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,18 @@
 <section class="header">
    <h2>Go Outside</h2>
 </section>
-<form>
+<!--
+<form method="post" action="WeatherController@load">
+-->
+{{ Form::open(array('action' => 'WeatherController@load')) }}
    <div class="row">
       <div class="six columns">
+         <!--
          <label for="cityInput">City Name</label>
          <input class="u-full-width" type="text">
+         -->
+         echo Form::label('cityInput', 'City Name');
+         echo Form::text('cityInput');
       </div>
       <div class="six columns">
          <label for=""></label>
