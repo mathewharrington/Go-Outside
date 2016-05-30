6439b38fb5ee9fb55fedda51662074591803bb2d
Mathew Harrington <mathewharrington@Mathews-MacBook-Pro.local>
2016-05-30 22:03:04 +1000

    added code to remove all whitespace from form input, it was messing with the request url's. Issue #19

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -25,21 +25,60 @@ class PagesController extends Controller
    */
    public function load(Request $request)
    {
-      $OpenWeatherClient = new \App\Pest\Pest(getenv('OPEN_WEATHER_BASE_URL'));
-      $OpenWeatherAPI = new \App\API_Adapter\OpenWeatherAPI($OpenWeatherClient);
-      $OpenWeatherResultSet = new \App\Result\OpenWeatherResultSet();
+      try
+      {
+         $OpenWeatherClient = new \App\Pest\Pest(getenv('OPEN_WEATHER_BASE_URL'));
+         $OpenWeatherAPI = new \App\API_Adapter\OpenWeatherAPI($OpenWeatherClient);
+         $OpenWeatherResultSet = new \App\Result\OpenWeatherResultSet();
 
-      // harcode for now, will come from form input
-      $OpenWeatherAPI->setParam("city", $request->input('city'));
-      $response = $OpenWeatherAPI->get();
-      $OpenWeatherResultSet->addResults($response);
-      $OpenWeatherResultSet->parse();
+         // cleanup form value
+         $city = str_replace(' ','', $request->input('city'));
+
+         // extract city from request object
+         $OpenWeatherAPI->setParam("city", $city);
+         $response = $OpenWeatherAPI->get();
+         $OpenWeatherResultSet->addResults($response);
+         $OpenWeatherResultSet->parse();
+
+         /**
+         * BEGIN FLICKR CALLS
+         */
+
+         // setup objects to make flickr request
+         $FlickrClient = new \App\Pest\Pest(getenv('FLICKR_BASE_URL'));
+         $FlickrAPI = new \App\API_Adapter\FlickrAPI($FlickrClient);
+         $FlickrResultSet = new \App\Result\FlickrResultSet();
+
+         // add options for flickr query
+         $FlickrAPI->setParam("tags", $city);
+         $FlickrAPI->setParam("per_page", 20);
+         $FlickrAPI->setParam("orientation", "landscape");
+
+         $flickrResponse = $FlickrAPI->get();
+         $FlickrResultSet->addResults($flickrResponse);
+         // this step is important - pest seems to add a few of its own characters to the response
+         $FlickrResultSet->parse();
+
+         $photoURLs = $FlickrResultSet->buildPhotoURLArray();
+
+         return view('welcome', ['photoResponse' => $photoURLs]);
+      }
+
+      catch(Pest_Exception $e)
+      {
+         return view('welcome');
+      }
+
+      catch(Exception $e)
+      {
+         return view('welcome');
+      }
 
       // build meaningful string
-      $weather = "Current weather for ";
-      $weather .= $OpenWeatherResultSet->getCityName();
-      $weather .= " is ";
-      $weather .= $OpenWeatherResultSet->getWeatherDesc();
-      return view('welcome', ['weatherResults' => $weather]);
+      // $weather = "Current weather for ";
+      // $weather .= $OpenWeatherResultSet->getCityName();
+      // $weather .= " is ";
+      // $weather .= $OpenWeatherResultSet->getWeatherDesc();
+      // return view('welcome', ['weatherResults' => $weather]);
    }
 }
